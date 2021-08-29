719a390dc9e0ea060913bf72a0b16d64bd01b1cd
Tyde Hashimoto <hasht795@hotmail.com>
2021-08-29 12:20:33 -0700

    Modify About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,24 +4,42 @@ import '../styles/AboutPage.scss';
 import { FaGithubSquare } from 'react-icons/fa';
 import { SiLinkedin } from 'react-icons/si';
 import { MdEmail } from 'react-icons/md';
+import { IconContext } from "react-icons";
 
 function AboutPage(props) {
     return (
     <div id='About-Page'>
         <div className='info-card'>
-            <img src={pfp} alt='Me'/>
-            <p>Tyde Hashimoto</p>
+            <img className='pfp' src={pfp} alt='Me'/>
+            <div className='name'>
+                <p>Tyde Hashimoto</p>
+            </div>
             <div className='icon-row'>
-                <MdEmail />
-                <FaGithubSquare />
-                <SiLinkedin />
+                <a className='info-links' href='mailto: tyd@pdx.edu'>
+                    <IconContext.Provider value={{ className: "info-icon" }}>
+                        <MdEmail />
+                    </IconContext.Provider>
+                </a>
+                <a className='info-links' href='https://github.com/TDotH'>
+                    <IconContext.Provider value={{ className: "info-icon" }}>
+                        <FaGithubSquare />
+                    </IconContext.Provider>
+                </a>
+                <a className='info-links' href='https://www.linkedin.com/in/tyde-hashimoto-665456156/'>
+                    <IconContext.Provider value={{ className: "info-icon" }}>
+                        <SiLinkedin />
+                    </IconContext.Provider>
+                </a>
             </div>
         </div>
         <div className='about'>
             <h1>
                 About Me
             </h1>
-            <p>I'm a computer science major from Portland State University that's interested in a lot of things</p>
+            <p>
+                I'm a computer science gradutate from Portland State University. My experience ranges from working in Python to 
+                c++ to Java. 
+            </p>
         </div>
     </div>
     );
