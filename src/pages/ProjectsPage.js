7e8e83a02a576ff1288dd204a5d57f2cd1b7e784
Tyde Hashimoto <hasht795@hotmail.com>
2021-09-06 11:20:24 -0700

    Add Projects Page
    -Added some additional elements in projects list for future extended card description feature later down the road

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,10 +1,58 @@
 import React from "react";
 import '../styles/ProjectsPage.scss'
+import projects from '../components/projectsList.js';
+import { FiLink } from 'react-icons/fi';
+import { IconContext } from "react-icons";
+
+const linkTypes = {
+    'website': <FiLink />,
+}
 
 function ProjectsPage(props) {
     return (
         <div id={props.id} className='projects-page'>
-            <p>Projects will go here</p>
+            <div className='projects-header'>
+                <h1>Projects</h1>
+            </div>
+            <div className='projects-container'>
+                {projects.map((project, index) => {
+                    return (
+                    <div key={index} className='project'>
+                        <div style={{ borderBottomColor: project.colorTheme }} className='project-name'>
+                            <div className='project-image'>
+                                <img src={project.image} alt='project'/>
+                            </div>
+                            <p>{project.name}</p>
+                        </div>
+                        <div className='project-description'>
+                        {project.type}
+                            <div className='project-keywords'>
+                                <p>
+                                {project.keywords.map((keyword, index, arr) => {
+                                    let key = keyword
+                                    if (arr.length - 1 !== index) {
+                                        key += ' / ';
+                                    } 
+                                    return key;
+                                })}
+                                </p>
+                            </div>
+                            <div className='project-links'>
+                                {project.links.map((link, index) => {
+                                    return (
+                                    <a key={index} target="_blank" rel="noopener noreferrer" className='info-links' href={link.link}>
+                                        <IconContext.Provider value={{ className: "info-icon" }}>
+                                            {linkTypes[link.type]}
+                                        </IconContext.Provider>
+                                    </a>
+                                    )
+                                })}
+                            </div>
+                        </div> 
+                    </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
